b72c22b8f1950b2c562694f921648b28e29df2f3
Prince Jan <janprince002@gmail.com>
2024-01-15 13:35:47 +0000

    checkpoint

--- exclusive/views.py
+++ exclusive/views.py
@@ -35,11 +35,17 @@ def lesson_detail(request, course_slug, lesson_slug):
     course = Course.objects.get(slug=course_slug)
     lesson = Lesson.objects.get(slug=lesson_slug)
     related_lessons = course.lessons.all().exclude(slug=lesson_slug)
+    next_lesson = lesson.lesson_number + 1
+    if next_lesson <= course.lessons.count():
+        next_lesson = course.lessons.get(lesson_number=next_lesson)
+    else:
+        next_lesson = None
 
     context = {
         'course': course,
         'lesson': lesson,
         'related_lessons': related_lessons,
+        'next_lesson': next_lesson,
     }
     return render(request, 'exclusive/single-lesson.html', context)
 
