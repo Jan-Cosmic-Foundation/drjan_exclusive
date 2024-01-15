4304446840b7ece172fd0389c2e3048760b72710
Prince Jan <janprince002@gmail.com>
2024-01-15 21:30:46 +0000

    fix settings debug

--- jiva_exclusive/settings.py
+++ jiva_exclusive/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ["DEBUG"]
+DEBUG = True if os.environ["DEBUG"] == 'True' else False
 
 ALLOWED_HOSTS = ["*"]
 
