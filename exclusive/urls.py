46e41b76a9b76a78e6b0b3d110047f61fd0e58b1
Prince Jan <janprince002@gmail.com>
2024-02-07 18:39:21 +0000

    add chnages

--- exclusive/urls.py
+++ exclusive/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 app_name = 'course'
 urlpatterns = [
-    path('', views.index, name='course_index'),
+    path('', views.course_index, name='course_index'),
+    path('index/', views.index, name='index'),
     path('courses/', views.courses, name='courses'),
     path('courses/<slug:course_slug>', views.course_detail, name='course_detail'),
     path('lessons/<slug:course_slug>', views.lessons, name='lessons'),
