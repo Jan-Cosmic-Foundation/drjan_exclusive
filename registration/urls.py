00aefebac6a3e22c84e262182a5b7941d1a32c43
Prince Jan <janprince002@gmail.com>
2024-04-14 14:02:46 +0000

    changes

--- registration/urls.py
+++ registration/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'registration'
 urlpatterns = [
     path('register/', views.index, name='register'),
+    path('payment/', views.payment, name='payment'),
+    path('checkout/', views.CheckoutView.as_view(), name='checkout'),
+    path('confirm-payment/', views.ConfirmPaymentView.as_view(), name='confirm-payment'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+import json
+import requests
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.conf import settings
 
 
 def register(request):
@@ -10,5 +14,72 @@ def index(request):
     if request.method == 'POST':
         data = request.POST
         print(data)
-        return HttpResponse('Data received')
+        return render(request, 'registration/payment.html')
     return render(request, 'registration/index.html')
+
+
+def payment(request):
+    return render(request, 'registration/payment.html')
+
+
+class CheckoutView(View):
+    template_name = 'exclusive/payment.html'
+
+    def get(self, request):
+        event = request.GET.get('event')
+        quantity = request.GET.get('quantity', 1)
+        amount = 200000 if event == 'main' else 10000 * int(quantity)
+        email = "somehing@gmail.com"
+
+        url = "https://api.paystack.co/transaction/initialize"
+
+        payload = json.dumps({
+            "email": email,
+            "amount": amount,
+            "callback_url": "https://awake.drbaffourjan.com/confirm-payment/",
+            "channels": [
+                "mobile_money"
+                "card",
+            ]
+        })
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {settings.PAYSTACK_SECRET_KEY}'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        response_data = response.json()
+
+        return redirect(response_data['data']['authorization_url'])
+
+
+class ConfirmPaymentView(View):
+    template_name = 'exclusive/confirm-payment.html'
+
+    def get(self, request):
+        context = {}
+        reference = request.GET.get('reference')
+        trxref = request.GET.get('trxref')
+
+        url = f"https://api.paystack.co/transaction/verify/{reference}"
+
+        headers = {
+            'Authorization': f'Bearer {settings.PAYSTACK_SECRET_KEY}',
+        }
+
+        response = requests.request("GET", url, headers=headers)
+        response_data = response.json()
+
+        status = response_data['data']['status']
+        amount = response_data['data']['amount']
+
+        context['status'] = status
+
+        if status == 'success':
+            user = request.user
+            user.profile.registered = True
+            user.profile.save()
+
+        print(context)
+        return render(request, self.template_name, context)
+
